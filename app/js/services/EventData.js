38cf31f56454c48e8026bcf3d4fb49554179588b
Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
2016-03-27 09:58:54 -0430

    add ngResource, get no retorna una promise, por lo que se pueden manejar los datos directo

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,7 +1,7 @@
-eventsApp.factory('eventData', function($http){    
+eventsApp.factory('eventData', function($resource){    
     return {
-        getEvent: function(successCallBack) {
-            return $http({method: 'GET', url: '/data/event/1'});                            
+        getEvent: function() {
+            return $resource('/data/event/:id', {id: '@id'}).get({id: 1});                            
         }        
     };
 });
