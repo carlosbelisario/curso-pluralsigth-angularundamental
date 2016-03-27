5c93a0f7fd066fb4dd80e422b1b4b2584255b06a
Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
2016-03-27 09:09:24 -0430

    add node controller, get data from server

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -1,10 +1,17 @@
 var express = require('express'),
     path = require('path'),
+    events = require('./eventsController'),
+    bodyParser = require('body-parser'),
     app = express(),
     rootPath = path.normalize(__dirname + '/../')
 ;    
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(rootPath + '/app'));
+
+app.get('/data/event/:id', events.get);
+app.post('/data/event/:id', events.save);
 app.listen(8000);
 
 console.log('Server running 8000 ...');
