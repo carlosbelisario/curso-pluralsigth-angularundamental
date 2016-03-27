7e8aa8915e1af26223a55f81fec0191ac2bc617a
Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
2016-03-27 16:12:53 -0430

    change to element directive, and template attribute

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,7 @@
 
 eventsApp.directive('mySample', function($compile) {
     return {
-        link: function(scope, element, attrs, controller) {
-            var markup = '<input type="text", ng-model="sampleData" /> {{sampleData}} <br />';
-            angular.element(element).html($compile(markup)(scope));
-            
-        }
+        restrict: 'E',
+        template: '<input type="text", ng-model="sampleData" /> {{sampleData}} <br />'
     }
 });
