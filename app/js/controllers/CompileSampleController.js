696486e1ffe60c8a1d3a7d53e276240ff6c35637
Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
2016-03-27 12:10:53 -0430

    parse service

--- app/js/controllers/CompileSampleController.js
+++ app/js/controllers/CompileSampleController.js
@@ -1,6 +1,21 @@
 'use strict';
 
-eventsApp.controller('CompileSampleController', function($scope, $compile) {
+eventsApp.controller('CompileSampleController', function($scope, $compile, $parse, $log) {
+    var fn = $parse('1 +2');
+    $log.info(fn());
+    
+    var getter = $parse('event.name'),
+        setter = getter.assign,
+        context = {event: {name: 'Angular Bot Camp'}},
+        context2 = {event: {name: 'Code Cap'}}        
+    ;
+    
+    $log.info(getter(context));
+    $log.info(getter(context2));
+
+    setter(context2, 'Other value');
+
+    $log.info(context2.event.name)
     $scope.appendDivToElement = function(markup) {
         return $compile(markup)($scope).appendTo(angular.element("#appendHere"));
     };
