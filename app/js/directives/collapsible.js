4ed670356ebfa95bce1e0117735e6c75ca999630
Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
2016-03-27 18:13:36 -0430

    transclude, explication

---
+++ app/js/directives/collapsible.js
@@ -0,0 +1,19 @@
+'use strict';
+
+eventsApp.directive('collapsible', function($compile) {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div><h4 class="well-title" ng-click="toggleVisibility()">{{title}}</h4><div ng-show="visible" ng-transclude></div></div>',
+        transclude: true,
+        controller: function($scope) {
+            $scope.visible = true;
+            $scope.toggleVisibility = function() {
+                $scope.visible = !$scope.visible;
+            }
+        },
+        scope: {
+            title: '@'
+        }
+    }
+});
