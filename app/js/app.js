f05ca973b4b23a96a9ecc0af010091dcc67fd8e7
Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
2016-03-27 13:24:41 -0430

    first route, new structure

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,12 @@
 'use strict';
 
-var eventsApp = angular.module('eventsApp', ['ngResource'])
+var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider.when('/newEvent', {
+            templateUrl: 'templates/event/NewEvent.html',
+            controller: 'EditEventController'
+        });
+    })
     .factory('myCache', function($cacheFactory) {
         return $cacheFactory('myCache', {capacity: 3});
     })
